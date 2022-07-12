a8441be6e991f4097712cea0df26675de721fad0
cys <rightbrain@163.com>
2022-07-12 19:02:36 +0800

    add code coverage

--- test/MTERC721Token.js
+++ test/MTERC721Token.js
@@ -3,13 +3,13 @@ const { ethers } = require('hardhat');
 
 describe('MTERC721Token contract', ()=> {
 
-    let Token, token, owner, addr1, addr2, addr3;
+    let Token, token, owner, addr1, addr2, addr3, addrs;
 
     beforeEach(async () => {
         Token = await hre.ethers.getContractFactory('MTERC721Token');
         token = await Token.deploy();
         await token.deployed();
-        [owner, addr1, addr2, addr3, _] = await ethers.getSigners();
+        [owner, addr1, addr2, addr3, ...addrs] = await ethers.getSigners();
         
     });
 
@@ -57,7 +57,7 @@ describe('MTERC721Token contract', ()=> {
     //     });
     // })
 
-    describe.only('setAllowList', ()=> {
+    describe('setAllowList', ()=> {
         it('Should be reverted because the caller is not owner', async ()=> {
             await expect(
                 token.connect(addr1).setAllowList([addr1.address], 10)
@@ -74,12 +74,12 @@ describe('MTERC721Token contract', ()=> {
                 await token.numAvailableToMint(addr2.address),
             ).to.equal(expectValue);
         });
-    })
+    });
 
     it('Should should set _allowList by owner', async ()=> {
         await token.connect(owner).setIsAllowListActive(true)
         const overrides = {
-            value: ethers.utils.parseEther('0.1231')
+            value: ethers.utils.parseEther('0.123')
         }
         await expect(
             token.connect(addr1).mintAllowList(1, overrides)
@@ -91,7 +91,7 @@ describe('MTERC721Token contract', ()=> {
         await expect(token.ownerOf(1)).to.be.revertedWith('ERC721: invalid token ID');
     });
 
-    describe.only('numAvailableToMint', () => {
+    describe('numAvailableToMint', () => {
         it('Should view numAvailableToMint', async ()=> {
             const expectValue = 5;
             await token.connect(owner)
@@ -102,5 +102,177 @@ describe('MTERC721Token contract', ()=> {
         });
     });
 
-    
+    describe('mintAllowList', () => {
+        it('Should be reverted because the isAllowListActive is false', async ()=> {
+            const overrides = {
+                value: ethers.utils.parseEther('0.123')
+            }
+            await token.connect(owner).setAllowList([addr1.address], 1)
+            await expect(
+                token.connect(addr1).mintAllowList(1, overrides)
+            ).to.be.revertedWith('Allow list is not active');
+        });
+
+        //spend long time as mint max_supply tokens
+        it('Should be reverted because exceeded max available to purchase', async ()=> {
+            await token.connect(owner).setIsAllowListActive(true);
+            const overrides = {
+                value: ethers.utils.parseEther('24.6')
+            }
+            for(let i = 0; i < 50; i++){
+                await token.connect(owner).setAllowList([addrs[i].address], 200)
+                await token.connect(addrs[i]).mintAllowList(200, overrides);
+            }
+            await token.connect(owner).setAllowList([addrs[50].address], 200)
+            await expect(
+                token.connect(addrs[50]).mintAllowList(1, overrides)
+            ).to.be.revertedWith('Purchase would exceed max tokens');
+        });
+        
+        it('Should be reverted because the caller do not have enough fund', async ()=> {
+            await token.connect(owner).setIsAllowListActive(true);
+            const overrides = {
+                value: ethers.utils.parseEther('0.122')
+            }
+            await token.connect(owner).setAllowList([addr1.address], 1)
+            await expect(
+                token.connect(addr1).mintAllowList(1, overrides)
+            ).to.be.revertedWith('Ether value sent is not correct');
+        });
+
+        it('Should mint token', async ()=> {
+            const baseUri = 'ipfs://test.url';
+            token.connect(owner).setBaseURI(baseUri);
+            await token.connect(owner).setIsAllowListActive(true);
+            const overrides = {
+                value: ethers.utils.parseEther('0.123')
+            }
+            await token.connect(owner).setAllowList([addr1.address], 1);
+            await token.connect(addr1).mintAllowList(1, overrides);
+            
+            expect(await token.tokenURI(0)).to.equal(baseUri + '0');
+            expect(await token.ownerOf(0)).to.equal(addr1.address);
+
+        });      
+    });
+
+    describe('setProvenance', () => {
+        it('Should be reverted because the caller is not owner', async ()=> {
+            await expect(
+                token.connect(addr1).setProvenance('random hash')
+            ).to.be.revertedWith('caller is not the owner');
+        });
+        it('Should should set PROVENANCE by owner', async ()=> {
+            const expectValue = 'random hash';
+            await token.connect(owner).setProvenance(expectValue);
+            expect(await token.PROVENANCE()).to.equal(expectValue);
+        })
+    });
+
+    describe('reserve', () => {
+        it('Should be reverted because the caller is not owner', async ()=> {
+            await expect(
+                token.connect(addr1).reserve(1)
+            ).to.be.revertedWith('caller is not the owner');
+        });
+        it('Should reserve tokens by owner', async ()=> {
+            const baseUri = 'ipfs://test.url/';
+            token.connect(owner).setBaseURI(baseUri);
+            await token.connect(owner).reserve(5);
+            for(let i =0; i < 5; i++){
+                expect(await token.tokenURI(i)).to.equal(baseUri + i);
+                expect(await token.ownerOf(i)).to.equal(owner.address);
+            }
+        })
+    });
+
+    describe('setSaleState', () => {
+        it('Should be reverted because the caller is not owner', async ()=> {
+            await expect(
+                token.connect(addr1).setSaleState(true)
+            ).to.be.revertedWith('caller is not the owner');
+        });
+        it('Should should set saleIsActive by owner', async ()=> {
+            const expectValue = true;
+            await token.connect(owner).setSaleState(expectValue);
+            expect(await token.saleIsActive()).to.equal(expectValue);
+        })
+    });
+
+    describe('mint', () => {
+        it('Should be reverted because the saleIsActive is false', async ()=> {
+            await token.connect(owner).setSaleState(false);
+            const overrides = {
+                value: ethers.utils.parseEther('0.123')
+            }
+            await expect(
+                token.connect(addr1).mint(1, overrides)
+            ).to.be.revertedWith('Sale must be active to mint tokens');
+        });
+
+         //spend long time as mint max_supply tokens
+         it('Should be reverted because exceeded max token', async ()=> {
+            await token.connect(owner).setSaleState(true);
+            const overrides = {
+                value: ethers.utils.parseEther('0.615')
+            }
+            // 5 token each time * 2000 = 10000
+            for(let i = 0; i < 2000; i++){
+                await token.connect(addr1).mint(5, overrides);
+            }
+            await expect(
+                token.connect(addr1).mint(1, overrides)
+            ).to.be.revertedWith('Purchase would exceed max tokens');
+        });
+        
+        it('Should be reverted because the caller do not have enough fund', async ()=> {
+            await token.connect(owner).setSaleState(true);
+            const overrides = {
+                value: ethers.utils.parseEther('0.122')
+            }
+            await token.connect(owner).setAllowList([addr1.address], 1)
+            await expect(
+                token.connect(addr1).mint(1, overrides)
+            ).to.be.revertedWith('Ether value sent is not correct');
+        });
+
+        it('Should mint token', async ()=> {
+            const baseUri = 'ipfs://test.url';
+            token.connect(owner).setBaseURI(baseUri);
+            await token.connect(owner).setSaleState(true);
+            const overrides = {
+                value: ethers.utils.parseEther('0.123')
+            }
+            await token.connect(addr1).mint(1, overrides);
+            
+            expect(await token.tokenURI(0)).to.equal(baseUri + '0');
+            expect(await token.ownerOf(0)).to.equal(addr1.address);
+
+        });      
+    });
+
+    describe('withdraw', ()=> {
+        it('Should be reverted because the caller is not owner', async ()=>{
+            await expect(
+                token.connect(addr1).withdraw()
+            ).to.be.revertedWith('caller is not the owner');
+        });
+        it('Should withdraw fund by the owner', async ()=> {
+            await token.connect(owner).setSaleState(true);
+            const overrides = {
+                value: ethers.utils.parseEther('5')
+            }
+            await token.connect(addr1).mint(1, overrides);
+            const accountBalanceBeforeWithdraw = ethers.utils.formatEther(
+                await token.provider.getBalance(owner.address)
+            )
+            await token.connect(owner).withdraw();
+            const accountBalanceAfterWithdraw = ethers.utils.formatEther(
+                await token.provider.getBalance(owner.address)
+            )
+            expect(
+                parseInt(accountBalanceAfterWithdraw) > parseInt(accountBalanceBeforeWithdraw)
+            ).to.be.true;
+        });
+    });
 });
